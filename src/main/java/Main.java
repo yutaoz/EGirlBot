57c1381973be2c9ae7143e06938c0ff61c5302c9
yutaoz <47333239+yutaoz@users.noreply.github.com>
2020-08-12 17:11:35 -0400

    Fixed missing bot token

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,7 +11,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws FileNotFoundException, LoginException {
         JDABuilder builder = new JDABuilder(AccountType.BOT);
-        File file = new File("\\src\\main\\resources\\token.txt");
+        File file = new File("src\\main\\resources\\token.txt");
         Scanner scan = new Scanner(file);
         String token = scan.nextLine();
         builder.setToken(token);
