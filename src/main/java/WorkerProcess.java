be6170a213844cf16f362e5c8bb10deb00052997
yutaoz <47333239+yutaoz@users.noreply.github.com>
2020-08-31 21:41:47 -0400

    added token directly to prevent file not found

--- src/main/java/WorkerProcess.java
+++ src/main/java/WorkerProcess.java
@@ -14,9 +14,10 @@ import java.util.Scanner;
 public class WorkerProcess {
     public static void main(String[] args) throws FileNotFoundException, LoginException {
         JDABuilder builder = new JDABuilder(AccountType.BOT);
-        File file = new File("src/main/resources/token.txt");
-        Scanner scan = new Scanner(file);
-        String token = scan.nextLine();
+        //File file = new File("src/main/resources/token.txt");
+        //Scanner scan = new Scanner(file);
+        //String token = scan.nextLine();
+        String token = "NzQzMjA0OTI0NDQ3NTIyOTQ3.XzRRhg.cE0--sbLt1GU8YrMD2oSrI-Gq-Y";
 
         builder.setToken(token);
         builder.addEventListener(new Chatter());
